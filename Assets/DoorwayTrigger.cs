1a88172b9ee747031311eff3ac793dacc0407440
Kingroy23 <sammyo.roychowdhury@gmail.com>
2021-03-20 15:12:52 -0700

    Started building Level 2

---
+++ Assets/DoorwayTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorwayTrigger : MonoBehaviour
+{
+    public GameObject Destination;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        collision.transform.position = Destination.transform.position;
+    }
+}
