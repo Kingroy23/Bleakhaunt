09b4c80df0ccfbbf365d263df1c27b8ad3e423fa
Kingroy23 <sammyo.roychowdhury@gmail.com>
2021-03-20 12:25:00 -0700

    Added player following script to Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, Time.deltaTime);
+    }
+}
