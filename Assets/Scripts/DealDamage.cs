4ec89c7be69fc2a37a02cac5810bb258318846b9
Kingroy23 <sammyo.roychowdhury@gmail.com>
2021-03-20 13:14:32 -0700

    Player now has hitpoints and can be damaged by spikes

---
+++ Assets/Scripts/DealDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour
+{
+    public int Attack = 1;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        damagable checkIfDamagable = collision.gameObject.GetComponent<damagable>();
+        //var checkIfDamagable = collision.GetComponent<Damagable>();
+        if (checkIfDamagable != null)
+        {
+            if (checkIfDamagable.isPlayer)
+            {
+                checkIfDamagable.Damage(Attack);
+            }
+        }
+
+        /*PlayerMovement motion = collision.gameObject.GetComponent<PlayerMovement>();
+        if (motion != null)
+        {
+            Vector2 bounceDirection = collision.relativeVelocity;
+            motion.movement = bounceDirection.normalized * -10f;
+
+        }*/
+    }
+}
---
+++ Assets/Scripts/damagable.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagable : MonoBehaviour
+{
+    public bool isPlayer = false;
+    
+ 
+    SpriteRenderer spriteRenderer;
+
+    float timer = -21f;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+
+    //need a variable to keep track of how many hit points we have
+    public int hitpoints = 3;
+
+    //I need a function which will let other things hurt me
+    public void Damage(int amount)
+    {
+        hitpoints -= amount;
+        timer = .8f;
+        spriteRenderer.color = Color.red;
+        Debug.Log("took " + amount + "damage, hp is " + hitpoints);
+    }
+    //I need to continuously check if I have any health points
+    // if no healthpoints, die
+    private void Update()
+    {
+        if (timer != -21f)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                spriteRenderer.color = Color.white;
+                timer = -21f;
+            }
+        }
+        /*if (hitpoints <= 0)
+        {
+            if (isPlayer)
+            {
+                //Load game over scene
+                GetComponent<LoadScene>().Load();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+        }*/
+    }
+}
